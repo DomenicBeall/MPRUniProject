e6f76a3761caa306259702a7f6fb5d626e494a5a
Domenic Beall <domenicbeall2@gmail.com>
2021-03-11 11:15:47 +1100

    Did all the work

--- mpr.js
+++ mpr.js
@@ -1,5 +1,6 @@
 const ddButton = document.getElementById("dropdown-head");
 const ddContent = document.getElementById("dropdown-content");
+const ddIcon = document.getElementById("dropdown-icon");
 
 ddButton.addEventListener("click", (event) => {
 
@@ -8,8 +9,10 @@ ddButton.addEventListener("click", (event) => {
     if (ddContent.style.display === "flex") {
         ddContent.style.display = "none";
         ddButton.style.borderRadius = "10px";
+        ddIcon.innerHTML = "&#11206;";
     } else {
         ddContent.style.display = "flex";
         ddButton.style.borderRadius = "10px 10px 0px 0px";
+        ddIcon.innerHTML = "&#11205;";
     }
 });
